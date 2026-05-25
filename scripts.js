a3954dedb7d4208ba67f848f0eaa9b5b33643b84
Denver John Demis <denverdemis@gmail.com>
2026-05-25 21:03:00 +0530

    form testing

--- scripts.js
+++ scripts.js
@@ -24,3 +24,42 @@ function revealOnScroll() {
 window.addEventListener("scroll", revealOnScroll);
 
 revealOnScroll();
+
+const form = document.getElementById("registrationForm");
+
+form.addEventListener("submit", async (e) => {
+
+  e.preventDefault();
+
+  const formData = new FormData(form);
+
+  const data = Object.fromEntries(formData.entries());
+
+  try {
+
+    const response = await fetch(
+      "https://script.google.com/a/macros/skyline.design/s/AKfycby6jxdHNLqAQZsfWLYsC29JZTxlwjsIihg8NrSpHFrcEL8RgsUdgzqqYC_4uFVuKGpo9Q/exec",
+      {
+        method: "POST",
+        body: JSON.stringify(data)
+      }
+    );
+
+    const result = await response.json();
+
+    if (result.result === "success") {
+
+      document.getElementById("successMessage")
+        .style.display = "block";
+
+      form.reset();
+    }
+
+  } catch (error) {
+
+    alert("Something went wrong.");
+
+    console.error(error);
+  }
+
+});
