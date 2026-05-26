3d64309a5fc5ae6acffbf9208e65509dc17b5111
Denver John Demis <denverdemis@gmail.com>
2026-05-26 23:00:59 +0530

    reveal issue

--- scripts.js
+++ scripts.js
@@ -62,3 +62,36 @@ form.addEventListener("submit", async (e) => {
   }
 
 });
+
+
+// =========================
+// SCROLL REVEAL
+// =========================
+
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+
+  (entries) => {
+
+    entries.forEach((entry) => {
+
+      if (entry.isIntersecting) {
+
+        entry.target.classList.add("active");
+
+      }
+
+    });
+
+  },
+
+  {
+    threshold: 0.15
+  }
+
+);
+
+reveals.forEach((el) => {
+  observer.observe(el);
+});
