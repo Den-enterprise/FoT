2ab59bb8ddfcefbea59041ac5611759031ab0680
Denver John Demis <denverdemis@gmail.com>
2026-05-28 19:35:03 +0530

    adding of sponsors and timer

--- scripts.js
+++ scripts.js
@@ -104,3 +104,52 @@ const observer = new IntersectionObserver(
 reveals.forEach((el) => {
   observer.observe(el);
 });
+
+
+const countdown =
+  document.getElementById("countdown");
+
+const deadline = new Date(
+  "May 31, 2026 23:59:59"
+).getTime();
+
+function updateCountdown() {
+
+  const now = new Date().getTime();
+
+  const distance = deadline - now;
+
+  if (distance < 0) {
+
+    countdown.innerHTML = "Closed";
+
+    return;
+  }
+
+  const days = Math.floor(
+    distance / (1000 * 60 * 60 * 24)
+  );
+
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24))
+    / (1000 * 60 * 60)
+  );
+
+  const minutes = Math.floor(
+    (distance % (1000 * 60 * 60))
+    / (1000 * 60)
+  );
+
+  const seconds = Math.floor(
+    (distance % (1000 * 60))
+    / 1000
+  );
+
+  countdown.innerHTML =
+
+    `${days}d ${hours}h ${minutes}m ${seconds}s`;
+}
+
+updateCountdown();
+
+setInterval(updateCountdown, 1000);
